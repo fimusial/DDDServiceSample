9884377096d5e73805ceeabb067b6b1c4b1e8983
Filip Musiał <fimusial@gmail.com>
2024-03-24 18:48:31 +0100

    data clearing command

--- docker-watch.ps1
+++ docker-watch.ps1
@@ -2,3 +2,5 @@ $db = docker ps --format '{{.ID}}' --filter "publish=5432"
 #$rabbitMQ = docker ps --format '{{.ID}}' --filter "publish=5672"
 
 watch 2 "docker exec -it $db psql -U postgres -d application -c 'select * from public.memo; select * from public.integrationeventoutbox;'" -clear
+
+#docker exec -it $db psql -U postgres -d application -c 'START TRANSACTION READ WRITE; delete from public.memo; delete from public.integrationeventoutbox; COMMIT;'
