eff3bf35470eb371fb14f02fca51e4f86b9e5a7b
Filip Musiał <fimusial@gmail.com>
2024-10-06 16:27:37 +0200

    better error handling when converting events to notifications

--- Application/RequestPipeline/DomainEventExtensions.cs
+++ Application/RequestPipeline/DomainEventExtensions.cs
@@ -4,28 +4,62 @@ using MediatR;
 
 namespace Application;
 
-public static class DomainEventExtensions
+public static class EventExtensions
 {
     public static INotification ToNotification(this IDomainEvent domainEvent)
     {
         INotification? notification = null;
+
 #pragma warning disable CA1031 // Do not catch general exception types
         try
         {
             var notificationGenericType = typeof(DomainEventNotification<>).MakeGenericType(domainEvent.GetType());
             notification = (INotification)Activator.CreateInstance(notificationGenericType, domainEvent)!;
         }
-        catch (Exception)
+        catch (Exception exception)
         {
+            ThrowCouldNotInstantiateNotification(domainEvent, exception);
         }
 #pragma warning restore CA1031 // Do not catch general exception types
 
         if (notification is null)
         {
-            var receivedEventTypeName = domainEvent?.GetType()?.FullName ?? "null";
-            throw new InvalidOperationException($"could not instantiate notification for domain event: {receivedEventTypeName}");
+            ThrowCouldNotInstantiateNotification(domainEvent);
         }
 
-        return notification;
+        return notification!;
+    }
+
+    public static INotification ToNotification(this IntegrationEvent integrationEvent)
+    {
+        INotification? notification = null;
+
+#pragma warning disable CA1031 // Do not catch general exception types
+        try
+        {
+            var notificationGenericType = typeof(IntegrationEventNotification<>).MakeGenericType(integrationEvent.GetType());
+            notification = (INotification)Activator.CreateInstance(notificationGenericType, integrationEvent)!;
+        }
+        catch (Exception exception)
+        {
+            ThrowCouldNotInstantiateNotification(integrationEvent, exception);
+        }
+#pragma warning restore CA1031 // Do not catch general exception types
+
+        if (notification is null)
+        {
+            ThrowCouldNotInstantiateNotification(integrationEvent);
+        }
+
+        return notification!;
+    }
+
+    private static void ThrowCouldNotInstantiateNotification(object? @event, Exception? exception = null)
+    {
+        var receivedEventTypeName = @event?.GetType().FullName ?? "null";
+
+        throw new InvalidOperationException(
+            $"could not instantiate notification for event: {receivedEventTypeName}",
+            exception);
     }
 }
